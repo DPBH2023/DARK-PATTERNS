c6cecb9f066371b6e3a8725418672220b4906efd
AjKing230 <119474802+AjKing230@users.noreply.github.com>
2024-01-27 17:30:00 +0530

    Add files via upload

---
+++ dark-patterns-recognition/api/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from joblib import load
+import matplotlib.pyplot as plt
+
+presence_classifier = load('category_classifier_rf.joblib')
+presence_vect = load('category_vectorizer_rf.joblib')
+category_classifier = load('presence_classifier_rf.joblib')
+category_vect = load('presence_vectorizer_rf.joblib')
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['POST'])
+def main():
+    if request.method == 'POST':
+        output = []
+        data = request.get_json().get('tokens')
+
+        for token in data:
+            result = presence_classifier.predict(presence_vect.transform([token]))
+            if result == 'Dark':
+                cat = category_classifier.predict(category_vect.transform([token]))
+                output.append(cat[0])
+            else:
+                output.append(result[0])
+        category_count = {}
+        for category in output:
+            if category in category_count:
+                category_count[category] += 1
+            else:
+                category_count[category] = 1
+
+        # Plotting the pie chart
+        labels = category_count.keys()
+        sizes = category_count.values()
+        plt.figure(figsize=(8, 8))
+        plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
+        plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        plt.title('Token Distribution')
+        plt.show()
+
+        dark = [data[i] for i in range(len(output)) if output[i] == 'Dark']
+        for d in dark:
+            print(d)
+        print()
+        print(len(dark))
+
+        message = '{ \'result\': ' + str(output) + ' }'
+        print(message)
+
+        json = jsonify(message)
+
+        return json
+
+if __name__ == '__main__':
+    app.run(threaded=True, debug=True)
