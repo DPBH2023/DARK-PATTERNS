b5f2b25395576e6c1f3cf01f7f875e8bcaa5b2e2
AjKing230 <119474802+AjKing230@users.noreply.github.com>
2024-01-30 20:11:06 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,22 +1,27 @@
+# Importing necessary modules
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from joblib import load
 import matplotlib.pyplot as plt
 
-presence_classifier = load('category_classifier_fr.joblib')
-presence_vect = load('category_vectorizer_fr.joblib')
-category_classifier = load('presence_classifier_fr.joblib')
-category_vect = load('presence_vectorizer_fr.joblib')
+# Loading trained models
+presence_classifier = load('category_classifier_bn.joblib')
+presence_vect = load('category_vectorizer_bn.joblib')
+category_classifier = load('presence_classifier_bn.joblib')
+category_vect = load('presence_vectorizer_bn.joblib')
 
+# Initializing Flask app
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # Enabling CORS for cross-origin requests
 
+# Define the main route for POST requests
 @app.route('/', methods=['POST'])
 def main():
     if request.method == 'POST':
-        output = []
-        data = request.get_json().get('tokens')
+        output = []  # List to store the predicted categories
+        data = request.get_json().get('tokens')  # Extracting tokens from JSON data
 
+        # Iterating over each token to predict its category
         for token in data:
             result = presence_classifier.predict(presence_vect.transform([token]))
             if result == 'Dark':
@@ -24,6 +29,8 @@ def main():
                 output.append(cat[0])
             else:
                 output.append(result[0])
+        
+        # Counting the occurrences of each category
         category_count = {}
         for category in output:
             if category in category_count:
@@ -31,21 +38,23 @@ def main():
             else:
                 category_count[category] = 1
 
-        # Plotting the pie chart
+        # Plotting the pie chart for token distribution
         labels = category_count.keys()
         sizes = category_count.values()
         plt.figure(figsize=(8, 8))
         plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
-        plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        plt.axis('equal')
         plt.title('Token Distribution')
         plt.show()
 
+        # Printing and counting tokens classified as 'Dark'
         dark = [data[i] for i in range(len(output)) if output[i] == 'Dark']
         for d in dark:
             print(d)
         print()
         print(len(dark))
 
+        # Creating JSON response
         message = '{ \'result\': ' + str(output) + ' }'
         print(message)
 
@@ -54,4 +63,4 @@ def main():
         return json
 
 if __name__ == '__main__':
-    app.run(threaded=True, debug=True)
+    app.run(threaded=True, debug=True)  # Running the Flask app
