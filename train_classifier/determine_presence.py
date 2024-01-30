4d690159ce8c1940205e3eb4bf67060d422504fa
AjKing230 <119474802+AjKing230@users.noreply.github.com>
2024-01-30 20:27:28 +0530

    Update determine_presence.py

--- train_classifier/determine_presence.py
+++ train_classifier/determine_presence.py
@@ -2,14 +2,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.pipeline import Pipeline
 from sklearn import metrics
 import joblib
 from joblib import dump
 
-df1 = pd.read_csv('/Users/kuwarjain/Desktop/DARK-PATTERNS/train_classifier/dataset2.csv')
-df2 = pd.read_csv('/Users/kuwarjain/Desktop/DARK-PATTERNS/train_classifier/dataset1.csv')
+df1 = pd.read_csv('train_classifier\ormie.csv')
+df2 = pd.read_csv('train_classifier\dark_patterns.csv')
 
 df1 = df1[pd.notnull(df1["Pattern String"])]
 df1 = df1[df1["classification"] == 0]
@@ -28,7 +28,7 @@ X_train, X_test, y_train, y_test = train_test_split(df['Pattern String'], df["cl
 text_clf = Pipeline([
     ('vect', CountVectorizer()),  # Convert text to a matrix of token counts
     ('tfidf', TfidfTransformer()),  # Transform a count matrix to a normalized tf-idf representation
-    ('clf', RandomForestClassifier()),  # Random Forest classifier
+    ('clf', BernoulliNB()),  # Bernoulli Naive Bayes classifier
 ])
 
 # Train the model
@@ -41,5 +41,5 @@ accuracy = metrics.accuracy_score(y_pred, y_test)
 print("Accuracy:", accuracy)
 
 # Save the classifier and vectorizer separately
-dump(text_clf.named_steps['clf'], 'presence_classifier_fr.joblib')
-dump(text_clf.named_steps['vect'], 'presence_vectorizer_fr.joblib')
+dump(text_clf.named_steps['clf'], 'presence_classifier_bn.joblib')
+dump(text_clf.named_steps['vect'], 'presence_vectorizer_bn.joblib')
